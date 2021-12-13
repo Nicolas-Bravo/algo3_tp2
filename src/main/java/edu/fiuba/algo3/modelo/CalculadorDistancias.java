698078bb011b9232f164069bb2a970289e7b8bf4
valentinthourte <vthourte@fi.uba.ar>
2021-12-13 18:43:38 -0300

    calculador distancias

--- src/main/java/edu/fiuba/algo3/modelo/CalculadorDistancias.java
+++ src/main/java/edu/fiuba/algo3/modelo/CalculadorDistancias.java
@@ -5,6 +5,37 @@ public class CalculadorDistancias {
 
 
     public int distancia(Destino origen, Destino destino) {
-        return 4500;
+        //return 4500;
+        return (int)calculadorDistancia(origen.latitud, origen.longitud, destino.latitud, destino.longitud, "K");
     }
+
+    private double calculadorDistancia(double lat1, double lon1, double lat2, double lon2, String unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") {
+            dist = dist * 1.609344;
+        } else if (unit == "N") {
+            dist = dist * 0.8684;
+        }
+        System.out.println(dist);
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
 }
