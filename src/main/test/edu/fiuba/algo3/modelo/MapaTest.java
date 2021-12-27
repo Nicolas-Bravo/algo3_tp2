7e1c2ceb8fab5b974ef4703416e56fe8edd5ce72
bravoNicolas <bravo.nicolasf@gmail.com>
2021-12-27 16:50:12 -0300

    Mapa Test

---
+++ src/main/test/edu/fiuba/algo3/modelo/MapaTest.java
@@ -0,0 +1,136 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.reloj.Reloj;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapaTest {
+
+    @Test
+    public void mapaConstuidoConUnDestinoLoTieneComoDestinoActual(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Mapa mapa = new Mapa(destinoUno);
+
+        assertEquals(destinoUno, mapa.destinoActual());
+    }
+
+    @Test
+    public void mapaEntraAUnEdificioDeSuCiudadActualYDemoraUnaHora(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Mapa mapa = new Mapa(destinoUno);
+        Reloj r = new Reloj();
+
+        mapa.entrar(r,edificioUno);
+
+        assertEquals("Lunes , 8:00", r.fechaActual());
+    }
+
+
+    @Test
+    public void mapaEntraAUnEdificioDeSuCiudadActualPorSegundaVezYDemoraDosHoras(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Mapa mapa = new Mapa(destinoUno);
+        Reloj r = new Reloj();
+
+        mapa.entrar(r,edificioUno);
+        mapa.entrar(r,edificioUno);
+
+        assertEquals("Lunes , 10:00", r.fechaActual());
+    }
+
+    @Test
+    public void mapaEntraAUnEdificioDeSuCiudadActualPorTerceraVezYDemoraTresHoras(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Mapa mapa = new Mapa(destinoUno);
+        Reloj r = new Reloj();
+
+        mapa.entrar(r,edificioUno);
+        mapa.entrar(r,edificioUno);
+        mapa.entrar(r,edificioUno);
+
+        assertEquals("Lunes , 13:00", r.fechaActual());
+    }
+
+    @Test
+    public void mapaEntraAUnEdificioDeSuCiudadActualYObtieneSuPista(){
+        Pista p = new Pista("pista de prueba");
+        Edificio edificioUno = new Edificio(p);
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Mapa mapa = new Mapa(destinoUno);
+        Reloj r = new Reloj();
+
+        mapa.entrar(r,edificioUno);
+
+        assertEquals(p, mapa.entrar(r,edificioUno));
+    }
+
+    @Test
+    public void mapaViajaAOtroDestinoYCambiaSuDestinoActual(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Edificio edificioDos = new Edificio(new Pista("otra pista aux"));
+        Destino destinoDos = new Destino("nombreDos",20.000,20.000,edificioDos);
+
+        Mapa mapa = new Mapa(destinoUno, destinoDos);
+
+        mapa.viajar(destinoDos);
+
+        assertEquals(destinoDos, mapa.destinoActual());
+    }
+
+    @Test
+    public void mapaViajaAOtroDestinoEntraAUnEdificioYDemoraUnaHora(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino destinoUno = new Destino("nombreUNo",10.000,10.000,edificioUno);
+
+        Edificio edificioDos = new Edificio(new Pista("otra pista aux"));
+        Destino destinoDos = new Destino("nombreDos",20.000,20.000,edificioDos);
+
+        Mapa mapa = new Mapa(destinoUno, destinoDos);
+        Reloj r = new Reloj();
+
+        mapa.viajar(destinoDos);
+        mapa.entrar(r, edificioDos);
+
+        assertEquals("Lunes , 8:00", r.fechaActual());
+    }
+
+    @Test
+    public void mapaViajaDesdeMontralAMexicoYDevuelDistancia3725km(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino montreal = new Destino("Montreal", 45.50884, -73.5878, edificioUno);
+
+        Edificio edificioDos = new Edificio(new Pista("otra pista aux"));
+        Destino mexico = new Destino("Mexico", 19.42847, -99.12766 , edificioDos);
+
+        Mapa mapa = new Mapa(montreal, mexico);
+
+        assertEquals(3725.00, mapa.viajar(mexico),10.00);
+    }
+
+
+    @Test
+    public void mapaViajaDesdeMontralALimaYDevuelDistancia6400km(){
+        Edificio edificioUno = new Edificio(new Pista("una pista aux"));
+        Destino montreal = new Destino("Montreal", 45.50884, -73.5878, edificioUno);
+
+        Edificio edificioDos = new Edificio(new Pista("otra pista aux"));
+        Destino lima = new Destino("Lima", -12.04318  , -77.02824 , edificioDos);
+
+        Mapa mapa = new Mapa(montreal, lima);
+
+        assertEquals(6400.00, mapa.viajar(lima),10.00);
+    }
+
+}
