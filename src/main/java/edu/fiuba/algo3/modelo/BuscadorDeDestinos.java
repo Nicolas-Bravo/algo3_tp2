be53b80633aa3807bd246aabf5c831e450c46816
bravoNicolas <bravo.nicolasf@gmail.com>
2021-12-28 19:40:43 -0300

    Cambio lector destinos

--- src/main/java/edu/fiuba/algo3/modelo/BuscadorDeDestinos.java
+++ src/main/java/edu/fiuba/algo3/modelo/BuscadorDeDestinos.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo;
 import edu.fiuba.algo3.modelo.archivos.DestinoCandidato;
 import edu.fiuba.algo3.modelo.archivos.LectorArchivoDestinos;
 import edu.fiuba.algo3.modelo.pista.Pista;
+import edu.fiuba.algo3.modelo.valor.Valor;
 
 import java.util.ArrayList;
 
@@ -10,9 +11,9 @@ public class BuscadorDeDestinos {
 
     private static ArrayList<DestinoCandidato> listaDestinos = LectorArchivoDestinos.generarListaDestinosCandidatos();
     
-    private static Destino generarSiguiente(Tesoro t, DestinoCandidato siguiente){
+    private static Destino generarSiguiente(Pista inicio, DestinoCandidato siguiente, Valor valor){
 
-        return new Destino(t.obtenerInicio().mostrarPista(), 10.00, 10.00, siguiente.obtenerEdificios(t.obtenerValor()));
+        return new Destino(inicio.mostrarPista(), 10.00, 10.00, siguiente.obtenerEdificios(valor));
     }
 
     public static RutaDeEscape obtenerRutaDeEscape(Tesoro tesoro) {
@@ -27,7 +28,7 @@ public class BuscadorDeDestinos {
         DestinoCandidato sig = listaDestinos.get((int) (Math.random() * listaDestinos.size()));
 
         for(int i = 0; i < tope; i++){
-            actual = generarSiguiente(tesoro, sig);
+            actual = generarSiguiente(nombre_actual, sig, tesoro.obtenerValor());
             destinoArray[i] = actual;
             nombre_actual = sig.obtenerNombre();
             sig = listaDestinos.get((int) (Math.random() * listaDestinos.size()));
