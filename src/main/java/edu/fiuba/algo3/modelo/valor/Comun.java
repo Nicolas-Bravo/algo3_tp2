ccc48401bf783be5dafbe306ab4ab510e7cd507d
bravoNicolas <bravo.nicolasf@gmail.com>
2021-12-29 00:21:16 -0300

    No se repiten edificios

--- src/main/java/edu/fiuba/algo3/modelo/valor/Comun.java
+++ src/main/java/edu/fiuba/algo3/modelo/valor/Comun.java
@@ -4,6 +4,7 @@ import edu.fiuba.algo3.modelo.Edificio;
 import edu.fiuba.algo3.modelo.pista.Pista;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Comun implements Valor {
     @Override
@@ -17,8 +18,19 @@ public class Comun implements Valor {
         edificiosArray = new Edificio[3];
         ArrayList<Pista> facil = descripcion.get(1);
 
+        Random random_method = new Random();
+        int index = random_method.nextInt(facil.size());
+        ArrayList<Integer> usados = new ArrayList<>();
+
+
+
         for(int i = 0; i < 3; i++){
-            edificiosArray[i] = facil.get((int) (Math.random() * facil.size())).obtenerEdificio();
+            edificiosArray[i] = facil.get(index).obtenerEdificio();
+            usados.add(index);
+            while (usados.contains(index)){
+                index = random_method.nextInt(facil.size());
+            }
+
         }
         return edificiosArray;
     }
--- src/main/java/edu/fiuba/algo3/modelo/valor/MuyValioso.java
+++ src/main/java/edu/fiuba/algo3/modelo/valor/MuyValioso.java
@@ -4,6 +4,7 @@ import edu.fiuba.algo3.modelo.Edificio;
 import edu.fiuba.algo3.modelo.pista.Pista;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MuyValioso implements Valor {
     @Override
@@ -15,10 +16,19 @@ public class MuyValioso implements Valor {
     public Edificio[] obtenerEdificios(ArrayList<ArrayList> descripcion) {
         Edificio[] edificiosArray;
         edificiosArray = new Edificio[3];
-        ArrayList<Pista> facil = descripcion.get(3);
+        ArrayList<Pista> dificil = descripcion.get(3);
+
+        Random random_method = new Random();
+        int index = random_method.nextInt(dificil.size());
+        ArrayList<Integer> usados = new ArrayList<>();
+
 
         for(int i = 0; i < 3; i++){
-            edificiosArray[i] = facil.get((int) (Math.random() * facil.size())).obtenerEdificio();
+            edificiosArray[i] = dificil.get(index).obtenerEdificio();
+            usados.add(index);
+            while (usados.contains(index)){
+                index = random_method.nextInt(dificil.size());
+            }
         }
         return edificiosArray;
     }
